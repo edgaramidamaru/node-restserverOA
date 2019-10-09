4fd8cb78b09abc5ca637134e132dac3fb32507dc
edgaramidamaru <edgarmorunocadima@gmail.com>
2019-10-09 03:17:41 -0400

    segundo commit

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,12 @@
 //PUERTO
 process.env.PORT = process.env.PORT || 3000;
+//entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+//bd
+let urlBD;
+if (process.env.NODE_ENV === 'dev') {
+    urlBD = 'mongodb://localhost:27017/cafe';
+} else {
+    urlBD = 'mongodb+srv://admin:admin@cluster0-pkrqo.mongodb.net/cafe';
+}
+process.env.URLDB = urlBD;
